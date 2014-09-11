57cd0fc08f5e8f23dfb38aad6b6443de81ce40a6
Daniel <Daniel@Daniel-PC>
2014-09-11 19:30:45 -0300

    problemas com Social Auth

--- src/java/bean/UserSessionBean.java
+++ src/java/bean/UserSessionBean.java
@@ -3,75 +3,58 @@ package bean;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.Map;
-import java.util.Properties;
+import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
-import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import org.brickred.socialauth.AuthProvider;
 import org.brickred.socialauth.Profile;
 import org.brickred.socialauth.SocialAuthConfig;
 import org.brickred.socialauth.SocialAuthManager;
-import org.brickred.socialauth.util.Base64.InputStream;
 import org.brickred.socialauth.util.SocialAuthUtil;
 
-@Named(value = "userSession")
+
+@ManagedBean(name = "userSession")
 @SessionScoped
 public class UserSessionBean implements Serializable {
-
+    
     private SocialAuthManager manager;
     private String originalURL;
     private String providerID;
     private Profile profile;
-
+    
     public UserSessionBean() {
         //... 
     }
-
+    
     public void socialConnect(String providerIDSelected) throws Exception {
         setProviderID(providerIDSelected);
         // Put your keys and secrets from the providers here
-        Properties props = System.getProperties();
-        props.put("graph.facebook.com.consumer_key", "654094184666610");
-        props.put("graph.facebook.com.consumer_secret", "07c5d082557431eeca769dab44a65f1b");
-        // Define your custom permission if needed
-        props.put("graph.facebook.com.custom_permissions", "publish_stream,email,user_birthday,user_location,offline_access");
-        // Initiate required components
-        SocialAuthConfig config = SocialAuthConfig.getDefault();
-        config.load(props);
+        SocialAuthConfig config = SocialAuthConfig.getDefault();        
+        String propUrl = "oauth_consumer.properties";
+        config.load(propUrl);
         manager = new SocialAuthManager();
         manager.setSocialAuthConfig(config);
         // 'successURL' is the page you'll be redirected to on successful login
-        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        String successURL = "http://localhost:8080/" + externalContext.getRequestContextPath() + "/faces/home.xhtml";
+        String successURL = "http://tophat.com.br/TopHatApp/faces/home.xhtml";
         String authenticationURL;
         authenticationURL = manager.getAuthenticationUrl(providerID, successURL);
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("authManager", manager);        
+        FacesContext.getCurrentInstance().responseComplete();
         FacesContext.getCurrentInstance().getExternalContext().redirect(authenticationURL);
         this.pullUserInfo();
-        ExternalContext ec;
-        FacesContext facesContext = FacesContext.getCurrentInstance();
-        if (facesContext == null) {
-            ec = null;
-        } else {
-            ec = facesContext.getExternalContext();
-        }
-        if (ec != null) {
-            Map attrMap = ec.getSessionMap();
-            if (attrMap != null) {
-                attrMap.put("userSession", this);
-            }
-        }
+        FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("userSession", this);
     }
-
-    public void pullUserInfo() {
+    
+    public void pullUserInfo() throws Exception {
         try {
             // Pull user's data from the provider
             ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
             HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
             Map map = SocialAuthUtil.getRequestParametersMap(request);
             if (this.manager != null) {
-                AuthProvider provider = (AuthProvider) manager.connect(map);
+                AuthProvider provider = manager.connect(map);
                 this.profile = provider.getUserProfile();
                 // Do what you want with the data (e.g. persist to the database, etc.)
                 System.out.println("User's Social profile: " + profile);
@@ -81,10 +64,10 @@ public class UserSessionBean implements Serializable {
                 FacesContext.getCurrentInstance().getExternalContext().redirect(externalContext.getRequestContextPath() + "home.xhtml");
             }
         } catch (Exception ex) {
-            System.out.println("UserSession - Exception: " + ex.toString());
+            throw ex;
         }
     }
-
+    
     public void logOut() {
         try {
             // Disconnect from the provider
@@ -98,11 +81,6 @@ public class UserSessionBean implements Serializable {
         } catch (IOException ex) {
             System.out.println("UserSessionBean - IOException: " + ex.toString());
         }
-    }
-
-    public void teste() throws IOException {
-        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
-        FacesContext.getCurrentInstance().getExternalContext().redirect(externalContext.getRequestContextPath() + "home.xhtml");
     }
 
     public SocialAuthManager getManager() {
@@ -136,43 +114,6 @@ public class UserSessionBean implements Serializable {
     public void setProfile(Profile profile) {
         this.profile = profile;
     }
-
-    /**
-     * Loads the application configuration from the given input stream Format of
-     * the input stream should be as follows: <br/> www.google.com.consumer_key
-     * = opensource.brickred.com
-     *
-     * @param inputStream property file input stream which contains the
-     * configuration.
-     * @throws Exception
-     */
-    public void load(final InputStream inputStream) throws Exception {
-    }
-
-    /**
-     * Loads the application configuration from the given file
-     *
-     * @param fileName the file name which contains the application
-     * configuration properties
-     * @throws Exception
-     */
-    public void load(final String fileName) throws Exception {
-    }
-
-    /**
-     * Loads the application properties from oauth_consumer.properties file.
-     *
-     * @throws Exception
-     */
-    public void load() throws Exception {
-    }
-
-    /**
-     * Loads the application configuration from the given properties
-     *
-     * @param properties application configuration properties
-     * @throws Exception
-     */
-    public void load(final Properties properties) throws Exception {
-    }
+    
+    
 }
