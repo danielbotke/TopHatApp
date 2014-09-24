da7d113b0b14130d4f1f03bb0f86b22a9eb04696
danielbotke <daniel.botke@gmail.com>
2014-09-24 18:39:18 -0300

    Recebendo pacote Udp do Arduino com sucesso

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,10 +1,11 @@
 
+import bean.TopHatMB;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import javax.faces.bean.ManagedBean;
+import org.slf4j.LoggerFactory;
 
 /*
  * To change this template, choose Tools | Templates
@@ -18,29 +19,32 @@ import javax.faces.bean.ManagedBean;
 public class Main {
 
     public void UDPListener() {
-        DatagramSocket datagramSocket = null;
-        try {
-            datagramSocket = new DatagramSocket(9090);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-
-        byte[] buffer = new byte[100];
-
-        // Create a datagram packet.
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-
-        while (true) {
-            // Receive the packet.
+        rx.start();
+    }
+    static Thread rx = new Thread() {
+        public void run() {
+            DatagramSocket sock = null;
+            byte[] msg = new byte[91];
+            DatagramPacket pack = new DatagramPacket(msg, msg.length);
             try {
-                datagramSocket.receive(packet);
-            } catch (IOException e) {
-                e.printStackTrace();
+                sock = new DatagramSocket(1901);
+            } catch (SocketException ex) {
+                org.slf4j.Logger logger = LoggerFactory.getLogger(TopHatMB.class);
+                logger.error(ex.getMessage(), ex);
             }
 
-            buffer = packet.getData();
-            // Print the data:
-            System.out.println(new String(buffer));
+
+            //Recebimento da mensagem
+            while (true) {
+                try {
+                    sock.receive(pack);
+                    String teste = new String(pack.getData()).trim();
+                    System.out.println("Pacote recebido: " + teste);
+                } catch (IOException ex) {
+                    org.slf4j.Logger logger = LoggerFactory.getLogger(TopHatMB.class);
+                    logger.error(ex.getMessage(), ex);
+                }
+            }
         }
-    }
+    };
 }
